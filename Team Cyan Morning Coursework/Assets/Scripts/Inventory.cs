a6257d92c72a24dd833a245530ec875208c95bca
pc807 <pc807@bath.ac.uk>
2021-11-20 16:23:09 +0000

    Added Inventory UI Functionality
    ****Currently****
    * Inventory UI can display up to 6 items
    * Will display stackable items with numbers.
    * Non stackable items will take up another slot.
    * Added a child object to player for more hit detection. Everything else still works as intended.
    
    ****Need to****
    * The UI needs to be able to scale with screen size.
    * Need option to use item from inventory by item click or button press.
    * Need to implement chest system + money system
    * Need the UI to persist with scene changes.

--- Team Cyan Morning Coursework/Assets/Scripts/Inventory.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Inventory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,29 +8,26 @@ using UnityEngine;
  */
 public class Inventory
 {
+    //Creates an event that will trigger when item is added to the inventory.
+    public event EventHandler onItemListChange;
+
     //A list of Loot to store items picked up by the player.
     private List<Loot> listOfItems;
 
     //Constructor to initialize the list.
     public Inventory() {
         listOfItems = new List<Loot>();
-
-        //TESTING FOR UI
-        //TESTING FOR UI
-        addItem(new Loot { lootType = Item.ItemType.Apple, lootAmount = 1 });
-        addItem(new Loot { lootType = Item.ItemType.Berry, lootAmount = 1 });
-        //TESTING FOR UI
-        //TESTING FOR UI
     }
 
     //A function to add items to the inventory.
-    public void addItem(Loot loot) {
+    public bool addItem(Loot loot) {
+        bool addSuccessful = false; //Loot has not been added to the inventory yet, so this is false by default.
         bool containsLoot = false; //Bool to indicate whether the item to be added is in the inventory already or not.
         int index = -1; //Index of the already added item.
         //Loops through the list and check if the item is already in the inventory.
         for (int i = 0; i < listOfItems.Count; i++)
         {
-            if (listOfItems[i].lootType == loot.lootType) 
+            if (listOfItems[i].lootType == loot.lootType)
             {
                 containsLoot = true;
                 index = i;
@@ -39,19 +37,44 @@ public class Inventory
         //If item not in inventory, add it.
         if (containsLoot == false)
         {
-            listOfItems.Add(loot);
+            //Max of 6 items in inventory.
+            if (listOfItems.Count < 6)
+            {
+                listOfItems.Add(loot);
+                addSuccessful = true;
+            }
         }
-        //If item is in inventory, increase the amount.
+        //If item is in inventory, do the following.
         else
         {
-            listOfItems[index].lootAmount = listOfItems[index].lootAmount + loot.lootAmount;
+            //If item is stackable, increase the amount.
+            if (loot.isStackable())
+            {
+                listOfItems[index].lootAmount = listOfItems[index].lootAmount + loot.lootAmount;
+                addSuccessful = true;
+            }
+            //If the item is not stackable, just add it to the inventory.
+            else
+            {
+                //Max of 6 items in inventory.
+                if (listOfItems.Count < 6)
+                {
+                    listOfItems.Add(loot);
+                    addSuccessful = true;
+                }
+            }
         }
+
+        //triggers the event. Which calls the function in Inventory_UI that refreshes the inventory UI.
+        onItemListChange?.Invoke(this, EventArgs.Empty);
 
         //Temp: Prints out the inventory list to console.
         for (int i = 0; i < listOfItems.Count; i++)
         {
             Debug.Log(listOfItems[i].lootType + ", " + listOfItems[i].lootAmount);
         }
+
+        return addSuccessful;
     }
 
     public List<Loot> getListOfItems() {
--- Team Cyan Morning Coursework/Assets/Scripts/Inventory_UI.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Inventory_UI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Inventory_UI : MonoBehaviour
 {
@@ -15,22 +17,64 @@ public class Inventory_UI : MonoBehaviour
 
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
+
+        //Subriscribe to the event in Inventory.
+        this.inventory.onItemListChange += Inventory_onItemListChange;
+        RefreshItemsInInventory();
+    }
+
+    //On triggering the even in Inventory, calls the function RefreshItemsInInventory().
+    private void Inventory_onItemListChange(object sender, System.EventArgs e) {
         RefreshItemsInInventory();
     }
 
     private void RefreshItemsInInventory() {
-        float x = 0.9f;
-        float y = -1.01f;
-        float cellSize = 20f;
-        foreach (Loot item in inventory.getListOfItems())
+        //Destroy all item slots in the item slot group, except the item_slot_1 object, which acts as a template.
+        foreach (Transform child in itemSlotGroup)
         {
+            if (child == itemSlot) {
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
+
+        float x = 0.95f; //Initial x posiiton
+        float y = -1.06f; //Initial y position
+        float cellSize = 19f; //size of the item slot.
+        //Loops through all items in the inventory.
+        foreach (Loot loot in inventory.getListOfItems())
+        {
+            //Instantiate a new item slot under the item slot group and set it to be active (not hidden)
             RectTransform itemSlotRectTransform = Instantiate(itemSlot, itemSlotGroup).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
+            //Set the position of the item slot in the UI.
             itemSlotRectTransform.anchoredPosition = new Vector2(x * cellSize, y * cellSize);
+
+            //Finds the icon image under the item slot.
+            Image image = itemSlotRectTransform.Find("Icon").GetComponent<Image>();
+            //Sets the image to the sprite of the item in the inventory.
+            image.sprite = loot.getSprite();
+
+            //Finds the Amount text under the item slot.
+            TextMeshProUGUI amountText = itemSlotRectTransform.Find("Amount").GetComponent<TextMeshProUGUI>();
+            //If the loot amount is greater than 1, set the text to the amount.
+            if (loot.lootAmount > 1)
+            {
+                amountText.SetText(loot.lootAmount.ToString());
+            }
+            //If the amount is 1 or less, hide the text by setting it to nothing.
+            else 
+            {
+                amountText.SetText("");
+            }
+
+            //Increments the x position
             x++;
-            if (x > 3) {
-                x = 0;
-                y++;
+            //If x is larger than 3
+            if (x > 3f) {
+                //set x back to 0 and increment y.
+                x = 0.95f;
+                y--;
             }
         }
     }
