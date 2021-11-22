0e823d0d836ae8bb042dde0520249dba3aad5a9e
Thomas Lawrie <tsl38@bath.ac.uk>
2021-11-22 18:26:01 +0000

    Finish Implementation of saving and loading player resources

--- Team Cyan Morning Coursework/Assets/Scripts/GameManager.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/GameManager.cs
@@ -7,57 +7,21 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    // Player resources
+    public Inventory playerInventory;       // Player's inventory
+    public int goldAmount;      // Player's gold
 
-    private void Awake()
+    void Awake()
     {
-        instance = this;
-        SceneManager.sceneLoaded += LoadState;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    // Resources
-    public List<Sprite> playerSprites;
-    public List<Sprite> weaponSprites;
-
-    // References
-    public Player player;
-
-    // Logic
-    public int apples;
-
-
-    // Save state
-    public void SaveState()
-    {
-        List<Loot> listOfItems = GameObject.Find("Player").GetComponent<Player>().playerInventory.GetListOfItems();
-        string s = "";
-
-        for (int i = 0; i < listOfItems.Count; i++)
+        if (instance == null)
         {
-            s += listOfItems[i].sprite + "|";
-            s += listOfItems[i].lootType + "|";
-            s += listOfItems[i].lootAmount + "|";
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+            playerInventory = new Inventory();
         }
-
-        PlayerPrefs.SetString("SaveState", s);
-    }
-
-    // Load state
-    public void LoadState(Scene s, LoadSceneMode mode)
-    {
-        if (!PlayerPrefs.HasKey("SaveState"))
-            return;
-
-        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
-
-        // Change player skin
-
-
-        // Load player's inventory
-        for (int i = 0; i < data.Length; i += 3)
+        else if (instance != this)
         {
-            //Loot tempLoot = new Loot { sprite = data[i], lootType = data[i+1], lootAmount = int.Parse(data[i+2]) };
-            //GameObject.Find("Player").GetComponent<Player>().playerInventory.AddItem();
+            Destroy(gameObject);
         }
     }
 }
--- Team Cyan Morning Coursework/Assets/Scripts/Player.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Player.cs
@@ -7,21 +7,37 @@ public class Player : Mover
     public Inventory playerInventory; //Inventory object.
     [SerializeField] private Inventory_UI inventoryUi;
 
-    private void Awake()
+    void Awake()
     {
-        //Initializes the player inventory.
-        playerInventory = new Inventory();
+        // Load the player resources
+        playerInventory = GameManager.instance.playerInventory;
+        gameObject.GetComponent<Gold_Amount>().goldAmount = GameManager.instance.goldAmount;
+
+        // Testing: print list of items
+        List<Loot> listOfItems = GameObject.Find("Player").GetComponent<Player>().playerInventory.GetListOfItems();
+        for (int i = 0; i < listOfItems.Count; i++)
+        {
+            Debug.Log(listOfItems[i].lootType + ", " + listOfItems[i].lootAmount);
+        }
+        // Initialise inventory UI
         if (inventoryUi != null)
         {
             inventoryUi.setInventory(playerInventory);
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal") * speed / 10;
         float y = Input.GetAxisRaw("Vertical") * speed / 10;
 
         UpdateMotor(new Vector3(x, y, 0));
+    }
+
+    // Save player's inventory to GameManager
+    public void SavePlayer()
+    {
+        GameManager.instance.playerInventory = playerInventory;
+        GameManager.instance.goldAmount = gameObject.GetComponent<Gold_Amount>().goldAmount;
     }
 }
