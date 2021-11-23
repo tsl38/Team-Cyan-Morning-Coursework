ac046f825e44b4785939ee53a1df122e3f9e59e1
Thomas Lawrie <tsl38@bath.ac.uk>
2021-11-23 23:25:35 +0000

    Create typewriter effect in dialogue box

--- Team Cyan Morning Coursework/Assets/Scripts/FloatingText.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/FloatingText.cs
@@ -7,6 +7,6 @@ public class FloatingText : MonoBehaviour
     [SerializeField] private TMP_Text textLabel;
     private void Start()
     {
-        textLabel.text = "The lady over there told me to go see you to get a potion of fire resistance\nThe lady over there told me to go see you to get a potion of fire resistance";
+        GetComponent<TypewriterEffect>().Run("The lady over there told me to go see you to get a potion of fire resistance\nThe lady over there told me to go see you to get a potion of fire resistance", textLabel);
     }
 }
