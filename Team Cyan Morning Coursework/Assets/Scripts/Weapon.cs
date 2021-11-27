0c12a833c14a3e317fd3c77ade3641d22230387e
pc807 <pc807@bath.ac.uk>
2021-11-27 20:04:03 +0000

    Add Enemies to Dungeon Level 1
    *****CURRENTLY*****
    *Added enemies and patrol routes to dungeon level 1
    *All enemies are stored as prefabs under prefabs/enemies folder.
    *Health values and damage values are set accordingly.
    *Enemy walking animation added.
    
    *****NEED*****
    *Someone needs to do the attacking animation for the enemies.

--- Team Cyan Morning Coursework/Assets/Scripts/Weapon.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Weapon.cs
@@ -26,7 +26,7 @@ public class Weapon : Collidable
 	{
 		base.Update();
 
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetMouseButtonDown(0))
 		{
 			if (Time.time - lastSwing > cooldown)
 			{
