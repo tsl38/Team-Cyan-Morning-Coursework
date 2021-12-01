f2cbcccc6340cdb8daa824db4a549ccfc0fca92b
pc807 <pc807@bath.ac.uk>
2021-12-01 23:46:26 +0000

    Add main menu (Not complete yet)
    *****Currently******
    * Added main menu for the game, it is not yet complete. But exit game works (after it's built) and start game works.
    * Added a temp transition from start menu to village level. (Test needs changing).
    * Changed the portal script so that it can now teleport the player to all levels, based on build order set in Unity.
    * Removed an unused script component from the portal object in the village map.
    * Added portal to dungeon 1 to teleport to final boss level.
    * Added transition between dungeon level 1 and final boss level (text requires changing).
    * Added a line of code to transition_onCollision.cs script so that the player is unable to move while the transition screen is active.

--- Team Cyan Morning Coursework/Assets/Scripts/Portal.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Portal.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : Collidable
 {
-    public string[] sceneNames;
     protected override void OnCollide(Collider2D boxCollider)
     {
         if (boxCollider.name == "Player")
         {
             // Save Player state
             GameObject.Find("Player").GetComponent<Player>().SavePlayer();
-            // Switch to Dungeon 1 Scene
-            string sceneName = sceneNames[1];
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-            Debug.Log("Entered Scene: " + sceneName);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
--- Team Cyan Morning Coursework/Assets/Scripts/Transition_onCollision.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/Transition_onCollision.cs
@@ -10,7 +10,11 @@ public class Transition_onCollision : Collidable
     {
         if (boxCollider.name == "Player")
         {
+            //Disables the player mover script until the transition screen is over.
+            GameObject.Find("Player").GetComponent<Mover>().enabled = false;
+            //Display the transition screen.
             GameObject.Find("Canvas").GetComponent<TransitionUI>().ShowTransition(transDialogue, 0);
+            //Deletes the object that colides with the player, so the transition screen cannot be triggered again.
             Destroy(GameObject.Find("Transition 1"));
         }
     }
