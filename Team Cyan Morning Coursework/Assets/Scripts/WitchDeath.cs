c43c80b517e248606b7b3b9c995f925e9d21fd8e
Thomas Lawrie <tsl38@bath.ac.uk>
2021-12-08 05:07:14 +0000

    Implement "Must defeat boss before TPing to next level"
    Also increase player speed for showcasing purposes

--- Team Cyan Morning Coursework/Assets/Scripts/WitchDeath.cs
+++ Team Cyan Morning Coursework/Assets/Scripts/WitchDeath.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class WitchDeath : MonoBehaviour
 {
-    [SerializeField] public GameObject endPoint;
     [SerializeField] private TMP_Text textLabel;
     [SerializeField] private DialogueList transDialogue;
 
     private void OnDestroy()
     {
-        //endPoint.SetActive(true);
-
         //Disables the player mover script until the transition screen is over.
         GameObject.Find("Player").GetComponent<Mover>().enabled = false;
         //Disables the weapon script for player.
